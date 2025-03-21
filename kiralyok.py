d0d6be7db19e52500e6f2cbac4fd85cfdb9f0e2b
juhasz-szabolcs <juhasz.szabolcs@szbi-pg.hu>
2025-03-21 13:30:36 +0100

    Mátyás király adatai

--- kiralyok.py
+++ kiralyok.py
@@ -4,10 +4,29 @@ cnx = connection.MySQLConnection(user='root', password='',
                                  host='127.0.0.1',
                                  database='kiralyok')
 
-
+# táblák megjelenítése
 cursor = cnx.cursor()
 cursor.execute("SHOW TABLES")
 for table in cursor:
     print(table)
+print("-------")
+
+# uralkodó megjelenítése
+cursor.execute("SELECT * FROM uralkodo")
+for uralkodo in cursor:
+    print(uralkodo)
+print("-------")
+
+# Mátyás király adatai:
+cursor.execute("SELECT * FROM uralkodo WHERE nev = 'I. Mátyás'")
+for uralkodo in cursor:
+    print(uralkodo)
+print("-------")
+
+# Mátyás király adatai:
+cursor.execute("SELECT szul, hal FROM uralkodo WHERE nev = 'I. Mátyás'")
+for uralkodo in cursor:
+    print(f"Mátyás király élt: {uralkodo[0]} - {uralkodo[1]}")
+print("-------")
 
 cnx.close()
